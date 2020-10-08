dafc3853b706c3500b6c7b2b065b291d4c835e0f
EduardoGuerraSanchez <eduardoguerra015@gmail.com>
2020-10-08 10:48:36 +0200

    Ejercicio 2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            int b = 4;
+            int sumar = a + b;
+            int restar = a - b;
+            int multiplicar = a * b;
+            float modulo = a % b;
+
+            Console.WriteLine(sumar);
+            Console.WriteLine(restar);
+            Console.WriteLine(multiplicar);
+            Console.WriteLine(modulo);
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
