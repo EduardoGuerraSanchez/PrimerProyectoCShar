96c1011e9bf5a3a7a786307356a3004944a2e462
EduardoGuerraSanchez <eduardoguerra015@gmail.com>
2020-10-08 10:57:54 +0200

    Ejercicio 3

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primerNumero;
+            int segundoNumero;
+            Console.WriteLine("Introduce tu primer numero");
+            primerNumero = System.Convert.ToInt32( System.Console.ReadLine());
+            Console.WriteLine("Introduce tu segundo numero");
+            segundoNumero = System.Convert.ToInt32(System.Console.ReadLine());
+
+            if (primerNumero > segundoNumero)
+            {
+                Console.WriteLine("El primer numero es mayor:");
+                Console.WriteLine(primerNumero);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("El segundo numero es mayor:");
+                Console.WriteLine(segundoNumero);
+                Console.ReadLine();
+            }
+        }
+    }
+}
