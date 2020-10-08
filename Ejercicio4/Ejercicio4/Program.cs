67101fdfc215666bd7e8abbd3c601049f688b2f6
EduardoGuerraSanchez <eduardoguerra015@gmail.com>
2020-10-08 11:02:57 +0200

    Ejercicio 4

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("Introduce el primer numero");
+            a = System.Convert.ToInt32(
+            System.Console.ReadLine());
+
+            Console.WriteLine("Introduce el segundo numero");
+            b = System.Convert.ToInt32(
+            System.Console.ReadLine());
+
+            if( (a % 2 == 0) && (b % 2 == 0))
+            {
+                Console.WriteLine("Son pares");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
