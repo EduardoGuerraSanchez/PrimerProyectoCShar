384cf980f67c4b04365e92fd3a581b5ac1ac5eac
EduardoGuerraSanchez <eduardoguerra015@gmail.com>
2020-10-08 10:14:29 +0200

    Primer HolaMundo

--- PrimerProyectoCShar/Program.cs
+++ PrimerProyectoCShar/Program.cs
@@ -10,6 +10,8 @@ namespace PrimerProyectoCShar
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola mundo");
+            Console.ReadLine();
         }
     }
 }
