af646682e56f0aa6708024523c24f55022fc6ce9
EduardoGuerraSanchez <eduardoguerra015@gmail.com>
2020-10-09 18:23:47 +0200

    Ejercicio 6

---
+++ Ejercicio6/Ejercicio6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int contador = 0;
+            char siete = '7';
+            Console.WriteLine("Introduce tu numero y a ver cuantos 7 tiene dicho numero");
+
+            string numero = Console.ReadLine();
+
+            foreach(char letra in numero)
+            {
+                Console.WriteLine(letra);
+                if( letra == siete){
+                    contador++;
+                }               
+            }
+            Console.WriteLine("Hay : " + contador + " sietes");
+            Console.ReadLine();
+        }
+    }
+}
