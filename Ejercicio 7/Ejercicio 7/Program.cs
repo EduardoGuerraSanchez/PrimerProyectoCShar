25ebaa653671c8b6b54cb7fca2dc1cdb0f50ad3f
EduardoGuerraSanchez <eduardoguerra015@gmail.com>
2020-10-10 18:36:40 +0200

    Ejercicio 7, a medio hacer

---
+++ Ejercicio 7/Ejercicio 7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fila = 0;
+            int columna = 0;
+            string N = "N";
+            string B = "B";
+            int salto = 0;
+            int i = 0;
+
+            Console.WriteLine("Introduce la primera posicion de tu alfil");
+            fila = System.Convert.ToInt32(System.Console.ReadLine());
+            columna = System.Convert.ToInt32(System.Console.ReadLine());
+
+
+            for(int contador=0; contador < 8; contador++)
+            {
+                Console.WriteLine(N+" "+B+" "+N+" "+B+" "+N+" "+B+" "+N+" "+B);
+                               
+            }
+            Console.ReadLine();
+
+            Console.WriteLine("La posicion de tu alfil se encuentra aqui");
+
+            
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
